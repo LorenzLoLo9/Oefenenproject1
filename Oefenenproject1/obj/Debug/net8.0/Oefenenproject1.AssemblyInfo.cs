aec48a07998abf329a53b7171eb8147ce51d9730
Lorenz <88002059+LorenzLoLo9@users.noreply.github.com>
2025-04-27 15:00:57 +0200

    First commit

--- Oefenenproject1/obj/Debug/net8.0/Oefenenproject1.AssemblyInfo.cs
+++ Oefenenproject1/obj/Debug/net8.0/Oefenenproject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oefenenproject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe26ddb6922e99676f018f38657c88c0dde812e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oefenenproject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oefenenproject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
